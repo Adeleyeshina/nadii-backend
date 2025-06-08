9ef754c4c1dd4bb56811f35842e4c8fd1209867f
Shina Adeleye <adeleyeshina4@gmail.com>
2025-06-08 00:00:04 -0700

    cors issue solved

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(cookieParser())
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : import.meta.env.FRONTEND_URI,
     methods : ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders : ['Content-Type', 'Authorization'],
     credentials : true
